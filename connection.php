cb45f2fbfe331f2841a9d9e3357b629e8d46bccf
Rohith <73929229+Kowndinyar@users.noreply.github.com>
2020-11-06 17:50:00 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -4,10 +4,9 @@ $username = "root";
 $password ="" ;
 $db="Registration";
 
-// Create connection
 $conn = mysqli_connect($servername, $username, $password, $db);
 
-// Check connection
+
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
